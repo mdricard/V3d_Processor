6ae06591883884f65f761fadf1dcc79726a240d2
Mark Ricard <ricard@uta.edu>
2025-05-19 11:52:12 -0500

    removed step len

--- v3d_Main.py
+++ v3d_Main.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Biomechanics import Biomechanics
 
-fn = 'D:/Alexis_Subject_27/S1 Data Test.txt'
-s_27_n_08 = Biomechanics(fn)
-s_27_n_08.get_stance()
+fn = 'C:/Alexis_Subject_7/S7 new data 2.txt'
+s_7 = Biomechanics(fn)
+s_7.get_stance()
 
 #s_27_n_08.plot_fz_steps()
-s_27_n_08.plot_joint_force()
-s_27_n_08.plot_joint_moment()
+s_7.plot_joint_force()
+s_7.plot_joint_moment()
