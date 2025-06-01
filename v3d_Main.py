ee5d923289c5c1676368a695135b312394d18aaf
Mark Ricard <ricard@uta.edu>
2025-06-01 15:48:19 -0500

    filename loop

--- v3d_Main.py
+++ v3d_Main.py
@@ -3,11 +3,34 @@ import matplotlib.pyplot as plt
 from Biomechanics import Biomechanics
 mass = 73.482
 height = 1.64592
-speed = 1.2
-incline = 'DH'
+#speed = 1.2
+#incline = 'DH'
 subject = 8
-shoe = 'HK'
-fn = 'D:/Alexis_Subject_8/S8 Data/S8 HK DH 12.txt'
+#shoe = 'HK'
+path = 'D:/Alexis_Subject_' + str(subject) + '/S' + str(subject) + ' Data/'
+for speed in range(3):
+    for incline in range(3):
+        for shoe in range(2):
+            if speed == 0:
+                speed_str = '05'
+            elif speed == 1:
+                speed_str = '08'
+            else:
+                speed_str = '12'
+            if incline == 0:
+                incline_str = 'Neutral'
+            elif incline == 1:
+                incline_str = 'UH'
+            else:
+                incline_str = 'DH'
+            if shoe == 0:
+                shoe_str = 'HK'
+            else:
+                shoe_str = 'Barefoot'
+            filename = path + 'S' + str(subject) + ' ' + shoe_str + ' ' + incline_str + ' ' + speed_str + '.txt'
+            fn = path + filename
+            print('Speed ' + speed_str + ' Incline ' + incline_str + ' Shoe ' + shoe_str + ' File ' + filename)
+"""
 s_7 = Biomechanics(fn, subject, mass, height, speed, incline, shoe)
 s_7.get_stance()
 #s_7.plot_joint_angle()
@@ -19,3 +42,5 @@ s_7.analyze_joint_force()
 #s_27_n_08.plot_fz_steps()
 #s_7.plot_joint_force()
 s_7.plot_joint_moment()
+
+"""
