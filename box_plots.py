8552a8099fb6f4c9968b5e5abbcc7390abd1ca7b
Mark Ricard <ricard@uta.edu>
2025-06-23 12:48:54 -0500

    added barefoot

--- box_plots.py
+++ box_plots.py
@@ -16,9 +16,19 @@ barefoot_uphill = barefoot[barefoot["incline"] == 1]
 barefoot_downhill = barefoot[barefoot["incline"] == 2]
 print(hoka.head())
 
+#--------------------------   Hoka  Level  -----------------------------
+ax = sns.boxplot(x="speed", y="add_mom",
+            hue='subject', legend=False,
+            data=hoka_level)
+sns.despine(offset=10, trim=True)
+ax.set_title("Hoka Neutral by Speed")        # Add title and labels
+ax.set_xticklabels(['0.5', '0.8', '1.2'])  # Custom
+ax.set_xlabel("Walking Speed (m/s)")
+ax.set_ylabel("Adduction Moment (%BWxHt)")
+plt.show()
 #--------------------------   Hoka  Uphill  -----------------------------
 ax = sns.boxplot(x="speed", y="add_mom",
-            hue='subject',
+            hue='subject', legend=False,
             data=hoka_uphill)
 sns.despine(offset=10, trim=True)
 ax.set_title("Hoka Uphill by Speed")        # Add title and labels
@@ -28,10 +38,42 @@ ax.set_ylabel("Adduction Moment (%BWxHt)")
 plt.show()
 #--------------------------   Hoka  Downhill  -----------------------------
 ax = sns.boxplot(x="speed", y="add_mom",
-            hue='subject',
+            hue='subject', legend=False,
             data=hoka_downhill)
 sns.despine(offset=10, trim=True)
 ax.set_title("Hoka Downhill by Speed")        # Add title and labels
+ax.set_xticklabels(['0.5', '0.8', '1.2'])  # Custom
+ax.set_xlabel("Walking Speed (m/s)")
+ax.set_ylabel("Adduction Moment (%BWxHt)")
+plt.show()
+
+
+#--------------------------   Barefoot  Level  -----------------------------
+ax = sns.boxplot(x="speed", y="add_mom",
+            hue='subject', legend=False,
+            data=barefoot_level)
+sns.despine(offset=10, trim=True)
+ax.set_title("Barefoot Neutral by Speed")        # Add title and labels
+ax.set_xticklabels(['0.5', '0.8', '1.2'])  # Custom
+ax.set_xlabel("Walking Speed (m/s)")
+ax.set_ylabel("Adduction Moment (%BWxHt)")
+plt.show()
+#--------------------------   Barefoot  Uphill  -----------------------------
+ax = sns.boxplot(x="speed", y="add_mom",
+            hue='subject', legend=False,
+            data=barefoot_uphill)
+sns.despine(offset=10, trim=True)
+ax.set_title("Barefoot Uphill by Speed")        # Add title and labels
+ax.set_xticklabels(['0.5', '0.8', '1.2'])  # Custom
+ax.set_xlabel("Walking Speed (m/s)")
+ax.set_ylabel("Adduction Moment (%BWxHt)")
+plt.show()
+#--------------------------   Barefoot  Downhill  -----------------------------
+ax = sns.boxplot(x="speed", y="add_mom",
+            hue='subject', legend=False,
+            data=barefoot_downhill)
+sns.despine(offset=10, trim=True)
+ax.set_title("Barefoot Downhill by Speed")        # Add title and labels
 ax.set_xticklabels(['0.5', '0.8', '1.2'])  # Custom
 ax.set_xlabel("Walking Speed (m/s)")
 ax.set_ylabel("Adduction Moment (%BWxHt)")
